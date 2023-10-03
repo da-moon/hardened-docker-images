c71b21fddb27b5e7d572382f206fd52b155f41ba
da-moon <contact@havi.dev>
2023-10-03 09:20:33 -0400

    chore: `commitlint` config initial commit
    Signed-off-by: da-moon <contact@havi.dev>

---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+// vim: filetype=js syntax=js softtabstop=2 tabstop=2 shiftwidth=2 fileencoding=utf-8 expandtab
+// code: language=js insertSpaces=true tabSize=2
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [0, "always", 72],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'release',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  }
+};

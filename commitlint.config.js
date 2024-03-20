0a5b6c6e77b98595de96596e789374b423f5da70
da-moon <contact@havi.dev>
2024-03-20 08:19:41 -0400

    chore: refactor and update commitlint configuration
    - Change string quotes from single to double in
      `commitlint.config.js`
    - Added rule for `body-max-line-length` in
      `commitlint.config.js`
    
    Signed-off-by: da-moon <contact@havi.dev>
    
    Precommit-Verified: 27f17b23742cf48158c3edfc13df48a74dec31000b2a0d1c2e5965a85b8938fd

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,26 +1,27 @@
 // vim: filetype=js syntax=js softtabstop=2 tabstop=2 shiftwidth=2 fileencoding=utf-8 expandtab
 // code: language=js insertSpaces=true tabSize=2
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    'header-max-length': [0, "always", 72],
-    'type-enum': [
+    "header-max-length": [0, "always", 72],
+    "body-max-line-length": [0, "always", "Infinity"],
+    "type-enum": [
       2,
-      'always',
+      "always",
       [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'release',
-        'revert',
-        'style',
-        'test',
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "release",
+        "revert",
+        "style",
+        "test",
       ],
     ],
-  }
+  },
 };

d145b020a5523dd4a3516ecd5f29e58aa74d68c1
da-moon <contact@havi.dev>
2024-03-22 07:47:44 -0400

    chore(lmanage): remove unnecessary comments
    Signed-off-by: da-moon <contact@havi.dev>
    
    Precommit-Verified: f8bb887cb5dcea48168cda439f369d4d4858921cf37313c8f86f7caa60e72f54

--- lmanage/build.sh
+++ lmanage/build.sh
@@ -20,7 +20,6 @@ export LOCAL
 IMAGE_NAME="${IMAGE_NAME:-$(basename "$(git rev-parse --show-prefix)")}"
 export IMAGE_NAME
 # ────────────────────────────────────────────────────────────
-# docker buildx rm --force  --all-inactive
 BUILDER="$(basename -s.git "$(git remote get-url origin)")"
 ! docker buildx inspect "${BUILDER}" 2>/dev/null && docker buildx create --bootstrap --name "${BUILDER}" --driver "docker-container"
 docker buildx use "${BUILDER}"

15866e46745c72a92ba62c937029a2d2f9dee73b
da-moon <contact@havi.dev>
2024-04-02 19:53:40 -0400

    build(powercli): implement Docker build script with SBOM generation
    - Add a new build script for the `powercli` project
    - The script checks if `docker buildx` and `docker sbom` are
      installed, and exits if not
    - Set the working directory to the script's location
    - Set `BUILDKIT_PROGRESS` and `LOCAL` environment variables,
      defaulting to "plain" and "true" respectively
    - Set `IMAGE_NAME` using the git repo prefix, if not set
      externally
    - Create a docker buildx builder if not already existing,
      using the git remote URL as the name
    - Use this builder to create a Docker image using `docker
      buildx bake`
    - Generate a software bill of materials (SBOM) for the image
      in SPDX JSON format
    - The script ends by returning to the original directory
    
    Signed-off-by: da-moon <contact@havi.dev>
    
    Precommit-Verified: 15a9a4e4b22143e889459e3d14602c39fc306ba59020c9979c49c653544b62cd

---
+++ powercli/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -xefuo pipefail
+if [[ $(docker buildx version >/dev/null 2>&1) ]]; then
+  echo >&2 "docker buildx is not installed"
+  exit 1
+fi
+if [[ $(docker sbom version >/dev/null 2>&1) ]]; then
+  echo >&2 "docker sbom is not installed"
+  exit 1
+fi
+WD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+pushd "${WD}" >/dev/null 2>&1
+# ────────────────────────────────────────────────────────────
+BUILDKIT_PROGRESS="${BUILDKIT_PROGRESS:-"plain"}"
+export BUILDKIT_PROGRESS
+
+LOCAL="${LOCAL:-"true"}"
+export LOCAL
+
+IMAGE_NAME="${IMAGE_NAME:-$(basename "$(git rev-parse --show-prefix)")}"
+export IMAGE_NAME
+# ────────────────────────────────────────────────────────────
+BUILDER="$(basename -s.git "$(git remote get-url origin)")"
+! docker buildx inspect "${BUILDER}" >/dev/null 2>&1 && docker buildx create --bootstrap --name "${BUILDER}" --driver "docker-container"
+docker buildx use "${BUILDER}"
+docker buildx bake
+docker sbom --format "spdx-json" --output "${IMAGE_NAME}.spdx.json" "${IMAGE_NAME}"
+popd >/dev/null 2>&1
